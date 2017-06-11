f27e7e2cc8130b8deb2fa9e8ce2c49ac34a87a79
Sophia Shapira <sophiaphillyqueen@users.noreply.github.com>
2017-06-10 22:25:22 -0400

    Created the launch file

---
+++ launch-file.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Category Feature Code
+ * Plugin URI: https://github.com/sophia-wordpressing/category-feature-code
+ * Description: Wordpress shortcodes to display components of posts of certain categories
+ * Version: 0.0.0.1
+ * Author: Sophia Elizabeth Shapira
+ * Author URI: https://sshapira.com
+ * License: GPL2+
+ */
+
